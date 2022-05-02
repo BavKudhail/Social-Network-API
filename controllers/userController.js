3eb651ea919565f7b39be06f2e7c596ec650a630
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-02 18:44:58 +0100

    built logic for getUserById and created corresponding route

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,31 @@ const { User, Thought } = require("../models");
 
 // create user controller variable
 
-const userController = {
+module.exports = {
   // get all users
   getAllUsers(req, res) {
     User.find({})
       .then((userData) => res.json(userData))
+      .catch((err) => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
+
+  //   get one user by their ID
+  getUserById({ params }, res) {
+    //   find one user
+    User.findOne({ _id: params.id })
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .then((userData) => {
+        if (!userData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(userData);
+      })
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
@@ -20,5 +40,3 @@ const userController = {
       .catch((err) => res.status(400).json(err));
   },
 };
-
-module.exports = userController;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,8 +1,15 @@
 const router = require("express").Router();
 
-const { getAllUsers, createUser } = require("../../controllers/userController");
+const {
+  getAllUsers,
+  createUser,
+  getUserById,
+} = require("../../controllers/userController");
 
-// GET / POST routes for /api/users
+//   /api/users
 router.route("/").get(getAllUsers).post(createUser);
+
+//   /api/users/:id
+router.route("/:id").get(getUserById);
 
 module.exports = router;
